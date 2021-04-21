bf43c4c8e0d2f4759e6c3e4b0bba50798be17e9f
Rizkyta <30465007+rshline@users.noreply.github.com>
2021-04-21 10:54:21 +0700

    resep

--- controllers/Resep.php
+++ controllers/Resep.php
@@ -1,6 +1,32 @@
 <?php
 
     class Resep extends Controller{
+        
+        public function __construct(){
+            if($_SESSION[''] != ''){
+                exit;
+            }
+        }
+
+        public function index(){
+            $data[''] = $this->model('ResepModel')->getAllResep();
+            $this->view('viewlistresep');
+        }
+
+        public function viewResep(){
+
+        }
+
+        public function tambahResep(){
+            $data[''] = $this->model('')->getAllResep();		
+            $this->view('viewlistresep', $data);
+        }
+
+        public function cari(){
+            $data['resep'] = $this->model('ResepModel')->cariResep();
+            $data['id'] = $_POST['id'];
+            $this->view('viewlistresep', $data);
+        }
 
     }
 
