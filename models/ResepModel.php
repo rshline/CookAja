f45ddcba84b9f0621e68a62a812db92b34a7fe77
Rizkyta <30465007+rshline@users.noreply.github.com>
2021-05-05 09:50:16 +0700

    Document code Rizkyta
    add document code

--- models/ResepModel.php
+++ models/ResepModel.php
@@ -1,28 +1,48 @@
 <?php
 
+/**
+ * Class Description
+ * Menerapkan kelas Resep berdasarkan class diagram
+ * 
+ * @author Rizkyta Shainy A.
+ */
     class ResepModel{
+
         private $id;
-        private $username;
+        private $nama;
         private $namaResep;
         private $kategori;
         private $bahan;
         private $tutorial;
         private $tgl;
         private $berbayar;
-    
+
+        /**
+         * Constructor
+         * Membuat object dari class Database baru 
+         */
         public function __construct(){
             $this->db = new Database;
         }
 
+        /**
+         * Mendapatkan array berisi data resep yang ada di database
+         * @return $this->db->resultSet() berisi data semua resep, may be null
+         */
         public function getAllResep(){
             $this->db->query("SELECT * FROM " . $this->table);
 		    return $this->db->resultSet();
         }
 
+        /**
+         * Menambahkan data input ke table resep di database
+         * @param data input
+         * @return $this->db->rowCount() yaitu jumlah data setelah dilakukan INSERT, not null
+         */
         public function tambahResep($data){
-            $query = "INSERT INTO resep (username, namaresep, bahan, tutorial, tgl, berbayar) VALUES(:namaresep, :bahan, :tutorial, :tgl, :berbayar)";
+            $query = "INSERT INTO resep (nama, namaresep, bahan, tutorial, tgl, berbayar) VALUES(:namaresep, :bahan, :tutorial, :tgl, :berbayar)";
             $this->db->query($query);
-            $this->db->bind('username', $data['username']);
+            $this->db->bind('nama', $data['nama']);
             $this->db->bind('namaresep', $data['namaresep']);
             $this->db->bind('bahan', $data['bahan']);
             $this->db->bind('tutorial', $data['tutorial']);
@@ -32,6 +52,11 @@
             return $this->db->rowCount();
         }
 
+        /**
+         * Memperbarui data resep pada database berdasarkan input
+         * @param data yang diinput
+         * @return $this->db->rowCount() yaitu jumlah data setelah dilakukan UPDATE, not null
+         */
         public function updateResep($data){
             $query = "UPDATE resep SET namaresep=:namaresep, bahan=:bahan, tutorial=:tutorial, tgl=:tgl, berbayar=:berbayar WHERE id=:id";
             $this->db->bind('namaresep', $data['namaresep']);
@@ -43,7 +68,12 @@
 
             return $this->db->rowCount();
         }
-        
+
+        /**
+         * Menghapus resep dengan id tertentu dari database
+         * @param id resep
+         * @return $this->db->rowCount() yaitu jumlah data setelah dilakukan DELETE, not null
+         */
         public function deleteResep($id){
             $this->db->query('DELETE FROM ' . $this->table . ' WHERE id=:id');
 		    $this->db->bind('id',$id);
@@ -51,7 +81,11 @@
 
 		    return $this->db->rowCount();
         }
-
+            
+        /**
+         * Mencari resep 
+         * @return  $this->db->resultSet() data yang memiliki key sesuai, may be null
+         */
         public function cariResep(){
             $key = $_POST['key'];
 		    $this->db->query("SELECT * FROM " . $this->table . " WHERE namaresep LIKE :key");
--- models/UserModel.php
+++ models/UserModel.php
@@ -1,21 +1,45 @@
 <?php
 
+/**
+ * Class Description
+ * Menerapkan kelas user berdasarkan kelas diagram
+ * 
+ * @author Rizkyta Shainy A.
+ */
+
 class UserModel {
 	
+	/**
+	 * @table String
+	 * @db adalah object
+	 */
 	private $table = 'user';
 	private $db;
 
+	/**
+	 * Constructor
+	 * Membuat object dari class Database baru 
+	 */
 	public function __construct()
 	{
 		$this->db = new Database;
 	}
 
+	/**
+	 * Mendapatkan array berisi data user yang ada di database
+	 * @return $this->db->resultSet() berisi data semua user, may be null
+	 */
 	public function getAllUser()
 	{
 		$this->db->query('SELECT * FROM ' . $this->table);
 		return $this->db->resultSet();
 	}
 
+	/**
+	 * Mendapatkan data user berdasarkan id dari database
+	 * @param id user yang dicari
+	 * @return array berisi data user, may be null
+	 */
 	public function getUserById($id)
 	{
 		$this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id');
@@ -23,6 +47,11 @@ class UserModel {
 		return $this->db->single();
 	}
 
+	/**
+	 * Menambahkan data input ke table user di database
+	 * @param data input
+	 * @return $this->db->rowCount() yaitu jumlah data setelah dilakukan INSERT, not null
+	 */
 	public function tambahUser($data)
 	{
 		$query = "INSERT INTO user (nama,username,password) VALUES(:nama,:username,:password)";
@@ -35,13 +64,23 @@ class UserModel {
 		return $this->db->rowCount();
 	}
 
-	public function cekUsername(){
+	/**
+	 * Mencari username
+	 * @param username
+	 * @return $this->db->single() data user yang usernamenya sesuai, may be null
+	 */
+	public function cekUsername($username){
 		$username = $_POST['username'];
 		$this->db->query("SELECT * FROM user WHERE username = :username");
 		$this->db->bind('username', $username);
 		return $this->db->single();
 	}
 
+	/**
+	 * Memperbarui data user pada database berdasarkan input
+	 * @param data yang diinput
+	 * @return $this->db->rowCount() yaitu jumlah data setelah dilakukan UPDATE, not null
+	 */
 	public function updateDataUser($data)
 	{
 		if(empty($_POST['password'])) {
@@ -62,6 +101,11 @@ class UserModel {
 		return $this->db->rowCount();
 	}
 
+	/**
+	 * Menhapus user dengan id tertentu dari database
+	 * @param id user
+	 * @return $this->db->rowCount() yaitu jumlah data setelah dilakukan DELETE, not null
+	 */
 	public function deleteUser($id)
 	{
 		$this->db->query('DELETE FROM ' . $this->table . ' WHERE id=:id');
@@ -71,11 +115,24 @@ class UserModel {
 		return $this->db->rowCount();
 	}
 
+	/**
+	 * Mencari user 
+	 * @return  $this->db->resultSet() data yang memiliki key sesuai, may be null
+	 */
 	public function cariUser()
 	{
 		$key = $_POST['key'];
 		$this->db->query("SELECT * FROM " . $this->table . " WHERE nama LIKE :key");
 		$this->db->bind('key',"%$key%");
 		return $this->db->resultSet();
+	}
+
+	/**
+	 * 
+	 */
+	public function getLogin(){
+		if(isset($_REQUEST('nama')) && isset ($_REQUEST('password'))){
+			
+		}
 	}
 }
